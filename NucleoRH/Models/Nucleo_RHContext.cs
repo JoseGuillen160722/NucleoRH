4ca9aa6a1604f80506894b582c4b3f8791bdfbbf
JoseGuillen160722 <programacion3@nucleodediagnostico.com>
2023-02-10 11:41:45 -0600

    .

--- NucleoRH/Models/Nucleo_RHContext.cs
+++ NucleoRH/Models/Nucleo_RHContext.cs
@@ -76,7 +76,7 @@ namespace NucleoRH.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=SRVNDDEV\\srvndbddev;Database=Nucleo_RH; Integrated Security=SSPI;");
+                optionsBuilder.UseSqlServer("Data Source=SRVNDDEV\\srvndbddev;Initial Catalog=Nucleo_RH;Persist Security Info=True;User Id=SIAN;Password=S14n2017");
             }
         }
 
